b79dd69472f6f92dbcb3a0e03ae1346817e57980
sofia_maidaniuk <ipz231_msye@student.ztu.edu.ua>
2025-03-13 20:33:55 +0200

    lab4

---
+++ lab2/task4-Prototype/ConsoleApp4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Prototype;
+
+class Program
+{
+    static void Main()
+    {
+        var ancestor = new Virus(10, 8, "Origin", "AlphaVirus");
+
+        var parent1 = new Virus(7, 5, "Delta", "BetaVirus");
+        var parent2 = new Virus(6, 4, "Gamma", "BetaVirus");
+
+        var child1 = new Virus(3, 3, "Lambda", "GammaVirus");
+        var child2 = new Virus(2, 2, "Omicron", "GammaVirus");
+        var child3 = new Virus(4, 2, "Sigma", "GammaVirus");
+
+        parent1.AddChild(child1);
+        parent2.AddChild(child2);
+        parent2.AddChild(child3);
+
+        ancestor.AddChild(parent1);
+        ancestor.AddChild(parent2);
+
+        Console.WriteLine("Original Virus Family");
+        Console.WriteLine("------------------------------------------------");
+        Console.WriteLine(ancestor.GetInfo());
+
+        // Глибоке клонування всієї ієрархії
+        Virus clone = ancestor.DeepClone();
+
+        Console.WriteLine("\nCloned Virus Family");
+        Console.WriteLine("------------------------------------------------");
+        Console.WriteLine(clone.GetInfo());
+
+        // Зміна параметрів клонованого вірусу
+        clone.Age = 12;
+        clone.Weight = 20;
+
+        Console.WriteLine("\nOriginal Virus Family (After Clone Change)");
+        Console.WriteLine("------------------------------------------------");
+        Console.WriteLine(ancestor.GetInfo());
+
+        Console.WriteLine("\nAltered Cloned Virus Family");
+        Console.WriteLine("------------------------------------------------");
+        Console.WriteLine(clone.GetInfo());
+    }
+}
---
+++ lab2/task4-Prototype/Prototype/Virus.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+    public class Virus : IPrototype<Virus>
+    {
+        public int Weight { get; set; }
+        public int Age { get; set; }
+        public string Name { get; }
+        public string Species { get; }
+        public Virus Parent { get; private set; } 
+
+        private HashSet<Virus> children;  // Унікальні діти
+
+        public Virus(int weight, int age, string name, string species)
+        {
+            Weight = weight;
+            Age = age;
+            Name = name;
+            Species = species;
+            children = new HashSet<Virus>();
+        }
+
+        public void AddChild(Virus child)
+        {
+            if (child != null)
+            {
+                child.Parent = this; // Призначення батьківського вірусу
+                children.Add(child);
+            }
+        }
+
+        public Virus DeepClone()
+        {
+            Virus clone = new Virus(Weight, Age, Name, Species);
+
+            foreach (Virus child in children)
+            {
+                clone.AddChild(child.DeepClone());
+            }
+
+            return clone;
+        }
+
+        public string GetInfo(int level = 0)
+        {
+            StringBuilder info = new StringBuilder();
+            string indent = new string(' ', level * 4); 
+            info.AppendLine($"{indent} Virus Info:")
+                .AppendLine($"{indent} Name: {Name}")
+                .AppendLine($"{indent} Species: {Species}")
+                .AppendLine($"{indent} Weight: {Weight}")
+                .AppendLine($"{indent} Age: {Age}");
+
+            if (Parent != null)
+                info.AppendLine($"{indent} Parent: {Parent.Name}");
+
+            info.AppendLine($"{indent} Children ({children.Count}):");
+
+            foreach (Virus child in children)
+            {
+                info.Append(child.GetInfo(level + 1));
+            }
+
+            return info.ToString();
+        }
+    }
+}
